62c8446abcb9e1a9beeb482019c194e9bb49744c
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-05-18 00:16:48 +0600

    Implement basic authentication with custom credentials

---
+++ src/main/java/com/example/basicauth/configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.basicauth.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated()) // Telling Spring Security to require authentication for all requests.
+                .httpBasic(withDefaults());                // Enable HTTP Basic Authentication by enabling BasicAuthenticationFilter.
+        return http.build();
+    }
+
+}
