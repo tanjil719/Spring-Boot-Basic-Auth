b7023452af124bfdfdc49e60f11953f38d2ec589
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-05-18 03:37:05 +0600

    Refactor UserController to improve import statements and remove unused dependencies

--- src/main/java/com/example/basicauth/controllers/UserController.java
+++ src/main/java/com/example/basicauth/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.example.basicauth.controllers;
 
 import com.example.basicauth.dtos.UserRegistrationDto;
-import com.example.basicauth.models.User;
 import com.example.basicauth.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
