34bfa9127b2f53d9b9ff01750d99fc7dc6470470
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-05-23 01:45:19 +0600

    Disable CSRF protection in SecurityFilterChain for simplified user registration and creation

--- src/main/java/com/example/basicauth/configs/SecurityConfig.java
+++ src/main/java/com/example/basicauth/configs/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,7 +26,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable)                                                            // Disable CSRF for simplicity
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/users/register", "/api/users/create").permitAll()  // Allow registration and user creation without authentication
                                 .anyRequest().authenticated())                                          // Require authentication for all other requests
