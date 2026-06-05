52123d1ecef479537c32b9915825763bc8052f89
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-06-05 16:58:24 +0600

    Add no-argument constructor to CustomPrincipal for improved flexibility

--- src/main/java/com/example/basicauth/payloads/CustomPrincipal.java
+++ src/main/java/com/example/basicauth/payloads/CustomPrincipal.java
@@ -3,11 +3,13 @@ package com.example.basicauth.payloads;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CustomPrincipal {
     private long userId;
     private String username;
